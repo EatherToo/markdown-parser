ea4e350338dae39eab858b09f4d669b997c4203f
Eather <eathertoo@gmail.com>
2022-11-25 15:16:07 +0800

    test: add titleTokenize test case

--- __test__/parser/tokenize/titleTokenize.test.ts
+++ __test__/parser/tokenize/titleTokenize.test.ts
@@ -5,26 +5,50 @@ import { Token, TokenType } from '../../../types/tokenize.type';
 describe('title tokenize test', () => {
   describe('test title token start', () => {
     it('md starts with `#`', () => {
-      const tokenBuffer = null;
-      const testRes = titleTokenize(tokenBuffer, 0, [], '#');
+      const testRes = titleTokenize(null, 0, [], '#');
       expect(testRes).toEqual(new TokenBuffer('#', TokenType.TitlePrefix, 0));
     });
 
+    it('token buffer is TitlePrefix', () => {
+      const tokenBuffer = new TokenBuffer('#', TokenType.TitlePrefix, 50);
+      const tokenRes = titleTokenize(tokenBuffer, 51, [], '#');
+      expect(tokenRes).toEqual(new TokenBuffer('##', TokenType.TitlePrefix, 51));
+    });
+
+    it('token buffer is not TitlePrefix', () => {
+      const tokenBuffer = new TokenBuffer('#', TokenType.Text, 50);
+      const tokenRes = titleTokenize(tokenBuffer, 51, [], '#');
+      expect(tokenRes).toEqual(new TokenBuffer('#', TokenType.Text, 51));
+    });
+
     it('token buffer closed', () => {
-      const tokenBuffer = null;
-      const testRes = titleTokenize(tokenBuffer, 0, [], ' ');
+      const testRes = titleTokenize(null, 0, [], ' ');
       const expectTokenBuffer = new TokenBuffer('#', TokenType.TitlePrefix, 0);
       expectTokenBuffer.position = 1;
       expectTokenBuffer.isClose = true;
       expectTokenBuffer.sourceText = '# ';
       expect(testRes).toEqual(expectTokenBuffer);
 
-      const testRes2 = titleTokenize(tokenBuffer, 0, [], '\t');
+      const testRes2 = titleTokenize(null, 0, [], '\t');
       const expectTokenBuffer2 = new TokenBuffer('#', TokenType.TitlePrefix, 0);
       expectTokenBuffer2.position = 1;
       expectTokenBuffer2.isClose = true;
       expectTokenBuffer2.sourceText = '#\t';
       expect(testRes2).toEqual(expectTokenBuffer2);
+    });
+  });
+
+  describe('token list is not empty', () => {
+    it('last token is white space type', () => {
+      const tokenList = [new Token(' ', 10, TokenType.Space)];
+      const tokenRes = titleTokenize(null, 11, tokenList, '#');
+      expect(tokenRes).toEqual(new TokenBuffer('#', TokenType.TitlePrefix, 11));
+    });
+
+    it('last token is not white space type', () => {
+      const tokenList = [new Token('#', 10, TokenType.Text)];
+      const tokenRes = titleTokenize(null, 11, tokenList, '#');
+      expect(tokenRes).toEqual(new TokenBuffer('#', TokenType.Text, 11));
     });
   });
 });
